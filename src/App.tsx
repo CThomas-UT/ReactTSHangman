f425217097b7cee1621fa8dc6d8da1990a5560da
CThomas-UT <conradt17@gmail.com>
2023-10-20 22:34:58 -0500

    Adds Wordlist and State Consts to App.tsx, closes #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import words from './chicagoWordList.json'
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+
+  const [guesses, setGuesses] = useState<string[]>([])
+
 
   return (
     <h1>Hi</h1>

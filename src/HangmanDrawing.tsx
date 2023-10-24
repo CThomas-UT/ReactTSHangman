2dc46f7e8d2c0a07e6f4da047a27a25c9848fa4a
CThomas-UT <conradt17@gmail.com>
2023-10-24 17:58:02 -0500

    Adds body components to HangmanDrawing function, closes #5

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,6 +1,87 @@
+const HEAD = (
+    <div style={{ 
+        height: "50px", 
+        width: "50px", 
+        background: "black", 
+        borderRadius: "50%",
+        border: "10px solid black",
+        position: "absolute",
+        top: "50px",
+        right: "-30px"}}/>
+)
+
+const BODY = (
+    <div style={{ 
+        height: "120px", 
+        width: "10px", 
+        background: "black", 
+        position: "absolute",
+        top: "120px",
+        right: "0px"}}/>
+)
+
+const LLEG = (
+    <div style={{ 
+        height: "90px", 
+        width: "10px", 
+        background: "black", 
+        position: "absolute",
+        transform: "rotate(45deg)",
+        transformOrigin: "top right",
+        top: "240px",
+        right: "0px"}}/>
+)
+
+const RLEG = (
+    <div style={{ 
+        height: "90px", 
+        width: "10px", 
+        background: "black", 
+        position: "absolute",
+        transform: "rotate(-45deg)",
+        transformOrigin: "top left",
+        top: "240px",
+        right: "0px"}}/>
+)
+
+const LARM = (
+    <div style={{ 
+        height: "90px", 
+        width: "10px", 
+        background: "black", 
+        position: "absolute",
+        transform: "rotate(130deg)",
+        transformOrigin: "top right",
+        top: "180px",
+        right: "10px"}}/>
+)
+
+const RARM = (
+    <div style={{ 
+        height: "90px", 
+        width: "10px", 
+        background: "black", 
+        position: "absolute",
+        transform: "rotate(-130deg)",
+        transformOrigin: "top left",
+        top: "180px",
+        right: "-10px"}}/>
+)
+
+
+
 export function HangmanDrawing() {
     return (
         <div style={{position: "relative"}}>
+            
+            {/* render hangman body */}
+            {HEAD}
+            {BODY}
+            {LLEG}
+            {RLEG}
+            {LARM}
+            {RARM}
+
             {/* render hangman pole */} 
 
             {/* Render rope */}

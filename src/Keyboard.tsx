7f27b402789aa5fa152f8b729dcb4f1e5d7035e2
CThomas-UT <conradt17@gmail.com>
2023-10-25 15:38:45 -0500

    Added in buttons for the keyboard, closes #9

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,3 +1,17 @@
-export function Keyboard() {
+const KEYS: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
+export function Keyboard() {
+    return (
+        <div style={{
+            display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(2em, 1fr))", gap: "0.25em",
+        }}>
+            {KEYS.map(key => {
+                return (
+                    <button key={key}>
+                        {key}
+                    </button>
+                )
+            })}
+        </div>
+    )
 }

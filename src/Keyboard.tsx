34448194f6327f6a3d94aab804a2ec5a3fa5afe4
CThomas-UT <conradt17@gmail.com>
2023-11-03 22:25:46 -0500

    basic keyboard styling with css module

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,13 +1,15 @@
+import styles from './Keyboard.module.css';
+
 const KEYS: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 export function Keyboard() {
     return (
         <div style={{
-            display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(2em, 1fr))", gap: "0.25em",
+            display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(2.5em, 1fr))", gap: "0.25em",
         }}>
             {KEYS.map(key => {
                 return (
-                    <button key={key}>
+                    <button key={key} className={styles.btn} >
                         {key}
                     </button>
                 )
